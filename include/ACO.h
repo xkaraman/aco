66607086d8715ca87010be6d216b9181294eece2
Xenakis <xenakiskaramanos@gmail.com>
2019-12-20 13:51:20 +0200

    First commit

---
+++ include/ACO.h
@@ -0,0 +1,62 @@
+#ifndef ACO_H
+#define ACO_H
+
+#include <vector>
+#include <string>
+#include <random>
+
+class ACOALgorithm {
+public:
+ACOALgorithm();
+ACOALgorithm(std::vector<std::vector<double> > distances);
+
+
+std::vector<int> getBestPath();
+double getBestLength();
+void setParameters(double maxIterations = 3000,
+                   double numAnts = 20,
+                   double explorationProbability = 0.9,
+                   double exhaustRatePheromones = 0.1,
+                   double transitionProbabilityA = 1,
+                   double transitionProbabilityB = 2,
+                   double transitionProbabilityX = 0.1);
+
+void runACS(const std::string& distancesFilename);
+void runACO();
+void runACO(const std::string& distancesFilename);
+
+private:
+std::vector<std::vector<double> > mDistances;
+std::vector<std::vector<double> > mDestinations;
+
+// ACO Parameters
+double mMaxIterations = 3000;
+double mNumAnts = 20;
+double mExplorationProbability = 0.9;
+double mExhaustRatePheromones = 0.1;
+double mTransitionProbabilityA = 1;
+double mTransitionProbabilityB = 2;
+double mTransitionProbabilityX = 0.1;
+
+int mWidth;
+int mHeight;
+bool mStopped = false;
+
+std::vector<int> mOptimal;
+double mOptimalLength;
+bool mCapacitated = false;
+
+std::vector<int> mDemand;
+std::vector<std::vector<int> > mBestList;
+
+std::random_device rd;
+std::mt19937 gen;
+
+// Helper functions
+std::vector< std::vector<double> > readCoords(std::string distancesFilename);
+std::vector< std::vector<double> > calcDistances(const std::vector<std::vector<double> > coords);
+
+
+};
+
+#endif
---
+++ src/ACO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void someFunc(){
+        return;
+}
+
+int main(int argc, char const *argv[]) {
+        int l;
+        int k;
+        k = 1;
+        l = 1;
+        
+
+        return 0;
+}
