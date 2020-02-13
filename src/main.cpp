e6f4a776a5c53b4bdee4ef15686bf162acfbcabb
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2020-02-13 14:51:47 +0200

    Update CMakeLists with Main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "../include/ACO.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    AntSystem ACO;
+
+    ACO.runACOFromFile("../data/sahara");
+    auto bestLength = ACO.getBestLength();
+    auto bestPath = ACO.getBestPath();
+
+    std::cout << "Best Length is: " << bestLength << '\n';
+    std::cout << "Best Path is: " << '\n';
+    for (size_t i = 0; i < bestPath.size(); i++) {
+        std::cout << bestPath[i] << ' ';
+    }
+
+    return 0;
+}
