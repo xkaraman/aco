51d470b1b3b96ecc45324f0e3a3568d17d858d14
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2020-02-23 19:49:29 +0200

    Source update

--- src/ACO.cpp
+++ src/ACO.cpp
@@ -66,20 +66,21 @@ void AntSystem::runACO()
 		return;
 	}
 
-	int numberDestinations = mDistances.size();
+	int numberDestinations = mDestinations.size();
 	std::vector<std::vector<double>> pheromoneDeposistedT(numberDestinations,std::vector<double>(numberDestinations,0));
 	std::vector<std::vector<double>> desirabilityTransitionH(numberDestinations,std::vector<double>(numberDestinations,0));
 
-	double t0 = 0;
+	double t0 = 0.0;
 	std::vector<int> bestTour(numberDestinations + 1);
 
 //	printVector("Coordinates:",mDestinations);
 //	printVector("Matrix Table:",mDistances);
 	for (int i = 0; i < pheromoneDeposistedT.size(); ++i) {
 		for (int j = 0; j < pheromoneDeposistedT[i].size(); ++j) {
-			if ( i == j) {
-				desirabilityTransitionH[i][j] = 1 / mDistances[i][j];
+			if (i == j) {
+				desirabilityTransitionH[i][j] = std::numeric_limits<double>::max();
 			}
+			desirabilityTransitionH[i][j] = 1 / mDistances[i][j];
 		}
 	}
 
@@ -163,7 +164,7 @@ void AntSystem::runACO()
 	int randomSolutions = 500;
 	for (int g = 0; g < randomSolutions; ++g) {
 		randomLength = 0;
-		std::vector<int> randomUnvisited;
+		std::vector<int> randomUnvisited(numberDestinations);
 		int start = dis(mGen);
 		std::vector<int> randomTour(numberDestinations+1);
 		randomTour[0] =  start;
@@ -203,21 +204,144 @@ void AntSystem::runACO()
 	std::vector<int> activeSolution(numberDestinations + 1);
 	activeSolution = bestTour;
 
+	std::cout << "Starting ACO Iterations..." << std::endl;
 	// ACO ITERATIONS
 	while (iteration < mMaxIterations) {
+		std::cout << "Iteration " << iteration << std::endl;
 		if(mStopped){
 			return;
 		}
 
 		std::vector<int> tourIteration(numberDestinations + 1);
-		double lengthIteration = std::pow(nearNeighbor,10);
+		double lengthIteration = std::pow(nearNeighbor,10.0);
 
+		int moves = 0;
+		for (int ant = 0; ant < mNumAnts; ++ant) {
+			moves = 0;
+			std::vector<int> tour(numberDestinations + 1);
+			std::uniform_int_distribution<int> dist(0,numberDestinations - 1);
+
+			tour[moves] = dist(mGen);
+
+			std::vector<int> unvisited(numberDestinations);
+			std::iota(unvisited.begin(),unvisited.end(),0);
+	        unvisited.erase(std::remove(unvisited.begin(),unvisited.end(),tour[0]),unvisited.end());
+
+	        printVector("Desirabilty", desirabilityTransitionH);
+	        for (int trip = 0; trip < numberDestinations - 1; ++trip) {
+				int c = tour[trip];
+				std::vector<double> choice;
+
+				for (int i = 0; i < unvisited.size(); ++i) {
+					int j = unvisited[i];
+					std::cout << "J:= " << j << " C:= " << c << std::endl;
+					std::cout << "PheromeT: " << pheromoneDeposistedT[c][j];
+					std::cout <<" DesirabilityH: " << desirabilityTransitionH[c].size() <<std::endl;
+					double value = std::pow(pheromoneDeposistedT[c][j], mTransitionProbabilityA ) * std::pow(desirabilityTransitionH[c][j], mTransitionProbabilityB);
+					choice.push_back(value);
+				}
+
+				std::uniform_real_distribution<double> unif(0,1);
+				double random1 = unif(mGen);
+
+				if (random1 < mExplorationProbability) {
+					int maxIndex = std::max_element(choice.begin(),choice.end()) - choice.begin();
+//					double maxValue = *std::max_element(choice.begin(), choice.end());
+					nextMove = unvisited[maxIndex];
+				} else {
+					std::vector<double> p;
+					sumP = 0;
+					for (int i = 0; i < unvisited.size(); ++i) {
+						int j = unvisited[i];
+						double value = (std::pow( pheromoneDeposistedT[c][j], mTransitionProbabilityA) * std::pow(desirabilityTransitionH[c][j], mTransitionProbabilityB));
+						sumP =+ j;
+						p.push_back(j);
+					}
+
+					double cumulativeSum = 0;
+					double randomNum = unif(mGen);
+
+					for (int i = 0; i < p.size(); ++i) {
+						p[i] = p[i] / sumP;
+						p[i] = cumulativeSum + p[i];
+						cumulativeSum = p[i];
+					}
+
+					for (int j = 0; j < p.size() - 1; ++j) {
+						if (randomNum >= p[j] && randomNum < p[j+1]) {
+							nextMove = unvisited[j];
+							break;
+						}
+
+					}
+
+				}
+
+				if (nextMove == c) {
+					nextMove = unvisited[0];
+				}
+
+				tour[trip + 1] = nextMove;
+		        unvisited.erase(std::remove(unvisited.begin(),unvisited.end(),tour[trip + 1]),unvisited.end());
+
+		        pheromoneDeposistedT[c][tour[trip+1]] = std::max(pheromoneDeposistedT[c][tour[trip + 1]] * (1 - mTransitionProbabilityX) + mTransitionProbabilityX * t0, tMin);
+			}
+
+	        tour[tour.size()-1] = tour[0];
+	        length = 0;
+	        for (int i = 0; i < tour.size() -1 ; ++i) {
+	        	length =+ mDistances[tour[i]][tour[i+1]];
+			}
+
+	        if (length < lengthIteration) {
+				tourIteration = tour;
+				lengthIteration = length;
+			}
+		}
+
+		lengthIteration = 0;
+		for (int i = 0; i < tourIteration.size() - 1; ++i) {
+			lengthIteration =+ mDistances[tourIteration[i]][tourIteration[i+1]];
+		}
+
+		if (lengthIteration < bestLength) {
+			bestLength = lengthIteration;
+			bestTour = tourIteration;
+		}
+
+		if (activeLength > lengthIteration) {
+			activeSolution = tourIteration;
+			activeLength = lengthIteration;
+		} else {
+			double C = lengthIteration - activeLength;
+			std::uniform_real_distribution<double> unif(0,1);
+			if (unif(mGen) < std::exp(-(float)C / (float)temperature)) {
+				activeSolution = tourIteration;
+				activeLength = lengthIteration;
+			}
+		}
+//
+		temperature = temperature * 0.999;
+
+		for (size_t i = 0; i < pheromoneDeposistedT.size(); i++) {
+			for (size_t j = 0; j < pheromoneDeposistedT[i].size(); j++) {
+				pheromoneDeposistedT[i][j] =  std::max(pheromoneDeposistedT[i][j] * (1 - pheromoneDeposistedT[i][j] / (tMin + tMax)), tMin);
+			}
+		}
+
+		tMax = (1 / ((1 - mExhaustRatePheromones))) * (1 / bestLength);
+	    tMin = tMax * (1 - std::pow(0.05, 1 / numberDestinations)) / ((numberDestinations / 2 - 1) * std::pow(0.05, 1 / numberDestinations));
+
+		for (int i = 0; i < activeSolution.size(); ++i) {
+			pheromoneDeposistedT[activeSolution[i]][activeSolution[i+1]] = std::min(pheromoneDeposistedT[activeSolution[i]][activeSolution[i + 1]] + (pheromoneDeposistedT[activeSolution[i]][activeSolution[i + 1]] / (tMax + tMin)) * (1 / activeLength), tMax);
+		}
+
+		results[iteration] = bestLength;
+		iteration++;
 	}
 
-
-
-
-
+	mOptimalPath = bestTour;
+	mOptimalLength = bestLength;
 
 }
 
