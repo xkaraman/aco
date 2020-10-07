2944dfe12f132d391c1540ceacd666a9cccfd32c
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2020-10-07 18:35:13 +0300

    Implement Methods / Fix randomness

--- src/Ant.cpp
+++ src/Ant.cpp
@@ -13,11 +13,14 @@
 
 #include "../include/utils.hpp"
 #include <cmath>
+#include <chrono>
 
 Ant::Ant():
-currentNode(){
+currentNode(),
+tourLength(),
+gen(randomDevice())
+{
 	// TODO Auto-generated constructor stub
-
 }
 
 Ant::~Ant() {
@@ -29,24 +32,40 @@ void Ant::init() {
 
 void Ant::init(int numOfDestinations) {
 	unvisited.resize(numOfDestinations);
-//	visited.resize(numOfDestinations);
-	probability.resize(numOfDestinations);
+	visited.reserve(numOfDestinations);
+	probability.reserve(numOfDestinations);
 	std::iota (std::begin(unvisited), std::end(unvisited), 0);
 	std::random_shuffle(unvisited.begin(),unvisited.end());
-
-	printVectorInt("\nANT::init", unvisited);
-
+//	printVectorInt("\nANT::init", unvisited);
 }
-void Ant::nextNode(const std::vector<std::vector<double>> &intensity,const std::vector<std::vector<double>> &visibility,const double &a,const double &b) {
-	// First node to visit
+
+void Ant::reset(const int numOfDestinations){
+	unvisited.clear();
+	unvisited.resize(numOfDestinations);
+	visited.clear();
+	visited.reserve(numOfDestinations);
+	probability.clear();
+	probability.reserve(numOfDestinations);
+	std::iota (std::begin(unvisited), std::end(unvisited), 0);
+	std::random_shuffle(unvisited.begin(),unvisited.end());
+	startNode();
+}
+
+void Ant::startNode(){
 	if(visited.empty()){
 		visited.push_back(unvisited.back());
 		unvisited.pop_back();
 		currentNode = visited.front();
-		printVectorInt("\nANT::nextNode::empty()", unvisited);
+//		printVectorInt("\nANT::startNode::empty()", unvisited);
 
 	}
-	else{
+}
+void Ant::computeTour(const std::vector<std::vector<double>> &intensity,const std::vector<std::vector<double>> &visibility,const double &a,const double &b) {
+//	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(randomDevice()); //Standard mersenne_twister_engine seeded with rd()
+	//		// Choose next node according to its probability
+	std::uniform_real_distribution<double> dis(0.0, 1.0);
+	while (!unvisited.empty()) {
 		double sum = 0.0;
 		std::vector<double> accumulatedSum;
 
@@ -56,33 +75,85 @@ void Ant::nextNode(const std::vector<std::vector<double>> &intensity,const std::
 			accumulatedSum.push_back(sum);
 		}
 
-		// Calcualte proability of each allowed Node
-		for (int i = 0 ; i < unvisited.size(); ++i) {
-			probability[i] = std::pow(intensity[currentNode][unvisited[i]],a) * std::pow(visibility[currentNode][unvisited[i]],b);;
-			probability[i] = probability[i] / sum ;
-			accumulatedSum[i] = accumulatedSum[i] / sum;
+		for (auto& allowed : unvisited ) {
+			probability.push_back((std::pow(intensity[currentNode][allowed],a) * std::pow(visibility[currentNode][allowed],b)) / sum);
 		}
 
-		printVector("\nANT:nextNode::notempty()",probability);
-		printVector("\nANT:nextNode::accu()",accumulatedSum);
-		// Choose next node according to its probability
-	    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-	    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-	    std::uniform_real_distribution<double> dis(0.0, 1.0);
+		for (auto& acc : accumulatedSum ) {
+			acc = acc / sum;
+		}
+		// Calcualte proability of each allowed Node
+		//		for (int i = 0 ; i < unvisited.size(); ++i) {
+		//			probability[i] = std::pow(intensity[currentNode][unvisited[i]],a) * std::pow(visibility[currentNode][unvisited[i]],b);;
+		//			probability[i] = probability[i] / sum ;
+		//			accumulatedSum[i] = accumulatedSum[i] / sum;
+		//		}
 
-	    double exceed = dis(gen);
-	    int ind = 0;
-	    std::cout << "\nSum to exceed: "<< exceed;
-	    std::cout << "\nAccumulated[index]: ";
+		//			printVector("\nANT:nextNode::notempty()",probability);
+		//			printVector("\nANT:nextNode::accu()",accumulatedSum);
 
-	    while(accumulatedSum[ind] < exceed){
-		    std::cout << "\nAccumulated[" << ind << "]: " << accumulatedSum[ind];
-	    	++ind;
+		double exceed = dis(gen);
+		int ind = 0;
+//			    std::cout << "\nSum to exceed: "<< exceed;
+		//	    std::cout << "\nAccumulated[index]: ";
 
-//	    	if(ind > accumulatedSum.size()){
-//	    		ind = 0;
-//	    	}
-	    }
-	    std::cout << "\nSelecting Node at index: " << ind << " with acccumulated sum " << accumulatedSum[ind];
+		while(accumulatedSum[ind] < exceed){
+			//		    std::cout << "\nAccumulated[" << ind << "]: " << accumulatedSum[ind];
+			++ind;
+
+			//	    	if(ind > accumulatedSum.size()){
+			//	    		ind = 0;
+			//	    	}
+		}
+//		std::cout << "\nSelecting Node at index: " << ind << " with acccumulated sum " << accumulatedSum[ind];
+//
+//		printVectorInt("\nANT::computeTour::notempty()::unvisitedBeforeChose", unvisited);
+//		printVectorInt("\nANT::computeTour::notempty()::visitedBeforeChose", visited);
+
+		currentNode = unvisited[ind];
+		visited.push_back(unvisited[ind]);
+		// REMOVE ELEMENT WITH VALUE 'ind' FROM VECTOR unvisited
+		unvisited.erase(std::remove(unvisited.begin(),unvisited.end(),unvisited[ind]),unvisited.end());
+
+//		printVectorInt("\nANT::computeTour::notempty()::unvisitedAfterChose", unvisited);
+//		printVectorInt("\nANT::computeTour::notempty()::visitedAfterchose", visited);
 	}
+}
+
+double Ant::computeTourLength(
+		const std::vector<std::vector<double> > &distances) {
+	double length = 0;
+
+//	printVectorInt("\nANT::computeTourLength", visited);
+	for (auto it = visited.begin(); it != visited.end(); ){
+	    int current = *it;
+	    int next = *it;
+
+	    if( it == visited.end()-1){
+		    next = *visited.begin();
+		    ++it;
+	    }
+	    else {
+	    	next = (*++it);
+	    }
+//		std::cout << "\nDistance at Nodes " << current <<" and " <<  next << " is: " << distances[current][next];
+		length += distances[current][next];
+	}
+
+	tourLength = length;
+	return length;
+}
+
+std::random_device Ant::randomDevice;
+
+bool Ant::edgeExists(const int i,const int j) const{
+	auto it = std::find(visited.begin(),visited.end(),i);
+	return (*++it) == j;
+}
+std::vector<int> Ant::getTour() const {
+	return visited;
+}
+
+double Ant::getTourLength() const {
+	return tourLength;
 }
--- src/Ant.h
+++ src/Ant.h
@@ -9,20 +9,31 @@
 #define SRC_ANT_H_
 
 #include <vector>
-
+#include <random>
 
 class Ant {
 public:
 	Ant();
+	Ant(const Ant&) = default;
 	virtual ~Ant();
 
 	void init();
 	void init(int numOfDestinations);
 
-	void pickNode();
+	void startNode();
+	void computeTour(const std::vector<std::vector<double>> &intensity,const std::vector<std::vector<double>> &visibility,const double &a,const double &b);
+	double computeTourLength(const std::vector<std::vector<double>> &distances);
+	bool edgeExists(const int i,const int j) const;
+	void reset(const int numOfDestinations);
 
-	void nextNode(const std::vector<std::vector<double>> &intensity,const std::vector<std::vector<double>> &visibility,const double &a,const double &b);
+	std::vector<int>	getTour() const;
+	double 				getTourLength() const;
+
 	float calculateTransisionProbability();
+
+	static std::random_device randomDevice;
+
+protected:
 
 private:
 	std::vector<int> 	unvisited;
@@ -30,6 +41,8 @@ private:
 	std::vector<double> probability;
 
 	int currentNode;
-};
+	double tourLength;
 
+	std::mt19937_64 gen;
+};
 #endif /* SRC_ANT_H_ */
